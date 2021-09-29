c32db5cb5f7ab4b2bbc1eb9fbe3581b18877afe4
belenmoreno-github <80197429+belenmoreno-github@users.noreply.github.com>
2021-09-29 17:55:49 +0200

    prueba

---
+++ practica 2.1/index2.js
@@ -0,0 +1,68 @@
+const vaciar = () => {
+document.getElementById("pantalla").value = "";
+}
+
+const cuadrado = () => {
+let num = document.getElementById("pantalla");
+num.value = num.value * num.value;
+}
+
+const cubo = () => {
+    let num = document.getElementById("pantalla");
+    num.value = num.value * num.value * num.value;
+  }
+
+const Mod = () => {
+let num = document.getElementById("pantalla");
+num.value = num.value * num.value;
+}
+
+const n = () => {
+let num = document.getElementById("pantalla");
+num.value = num.value * num.value;
+}
+
+const suma = () => {
+let num = document.getElementById("pantalla");
+num.value += num.value;
+}
+
+const resta = () => {
+    let num = document.getElementById("pantalla");
+    num.value += num.value;
+    }
+
+const multiplicacion = () => {
+let num = document.getElementById("pantalla");
+num.value = num.value * num.value * num.value;
+}
+
+const division = () => {
+    let num = document.getElementById("pantalla");
+    num.value = num.value * num.value * num.value;
+    }
+
+const igual = () => {
+let num = document.getElementById("pantalla");
+num.value = num.value * num.value * num.value;
+}
+
+const sumatorio = () => {
+let num = document.getElementById("pantalla");
+num.value = num.value * num.value * num.value;
+}
+
+const ordenar = () => {
+let num = document.getElementById("pantalla");
+num.value = num.value * num.value * num.value;
+}
+
+const revertir = () => {
+let num = document.getElementById("pantalla");
+num.value = num.value * num.value * num.value;
+}
+
+const quitar = () => {
+let num = document.getElementById("pantalla");
+num.value = num.value * num.value * num.value;
+}
